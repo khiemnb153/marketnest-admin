3c970e4c2f18a1dceba62b95eb78c95feda4e50d
khiemnb153 <khiemnb153@gmail.com>
2024-12-10 21:22:55 +0700

    Modify auth as Admin

--- app/(auth)/you-are-not-a-shopkeeper/page.jsx
+++ app/(auth)/you-are-not-a-shopkeeper/page.jsx
@@ -3,23 +3,18 @@
 import { signOut } from 'next-auth/react'
 
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@components/ui/card'
 
 const YouAreNotAShopkeeperPage = () => {
   return (
     <div className='flex h-screen w-full items-center justify-center px-4'>
       <Card className='mx-auto max-w-sm'>
         <CardHeader>
-          <CardTitle className='text-2xl'>Bạn không phải là chủ một gian hàng</CardTitle>
+          <CardTitle className='text-2xl'>Bạn không phải quản trị viên</CardTitle>
           <CardDescription>Đăng nhập bị từ chối</CardDescription>
         </CardHeader>
         <CardContent className='flex flex-col gap-4'>
-          <p>Nếu bạn muốn bắt đầu bán hàng trên Market Nest</p>
-          <Button asChild>
-            <Link href='#'>Đăng ký bán hàng</Link>
-          </Button>
-
           <Button
             onClick={() => signOut({ callbackUrl: '/logout' })}
             variant='destructive'
